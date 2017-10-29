d29407f9f59be2d66077c3343f110f71f88520e0
2siwon <lee89640245@gmail.com>
2017-10-29 18:02:55 +0900

    post_list와 /post/ URL매칭

--- instagram/config/urls.py
+++ instagram/config/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from post import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^post_')
+    url(r'^post/$', views.post_list, name='post_list'),
 ]
 
 urlpatterns += static(
