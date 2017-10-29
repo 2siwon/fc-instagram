cfa511762eb89d68906af21ceb28bea54c9e3f0f
2siwon <lee89640245@gmail.com>
2017-10-29 17:59:30 +0900

    post_list view구현

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 def post_list(request):
-    pass
+    posts = Post.objects.all()
+
+    context = {
+        'posts' : posts,
+    }
+
+    return render(request, 'post/post_list.html', context)
+
