70c875d952405899ef3c027dae75a76f9ced7221
2siwon <lee89640245@gmail.com>
2017-10-30 02:26:35 +0900

    signup기능 구현

--- instagram/config/urls.py
+++ instagram/config/urls.py
@@ -18,7 +18,8 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from post import views
+from post import views as post_views
+from member import views as member_views
 
 urlpatterns = [
     # admin
@@ -26,20 +27,27 @@ urlpatterns = [
 
     # post
     url(r'^post/$',
-        views.post_list,
+        post_views.post_list,
         name='post_list'),
 
     url(r'^post/create/$',
-        views.post_create,
+        post_views.post_create,
         name='post_create'),
 
     url(r'^post/(?P<post_pk>\d+)/$',
-        views.post_detail,
+        post_views.post_detail,
         name='post_detail'),
 
     url(r'^post/(?P<post_pk>\d+)/comment/create/$',
-        views.comment_create,
+        post_views.comment_create,
         name='comment_create'),
+
+    # Member
+    url(r'^member/signup/$',
+        member_views.signup,
+        name='signup'),
+
+
 ]
 
 urlpatterns += static(
