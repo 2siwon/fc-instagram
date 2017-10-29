a6aadb03f5c6428a61f088b93ab97479cd54d567
2siwon <lee89640245@gmail.com>
2017-10-29 21:53:20 +0900

    Post생성에 PostForm을 사용

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import PostForm
 from .models import Post
 
 
@@ -15,7 +17,16 @@ def post_list(request):
 
 def post_create(request):
     if request.method == "POST":
-        print(request.POST)
-        print(request.FILES)
-    elif request.method == "GET":
+
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            post = Post.objects.create(
+                photo=form.cleaned_data['photo'])
+            return HttpResponse(f'<img src="{post.photo.url}">')
+        else:
+            return HttpResponse('Form invalid!')
+    else:
         return render(request, 'post/post_create.html')
+
+
